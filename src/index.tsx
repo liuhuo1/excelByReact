8a2bf4b00bd268174ec8193fb3ea5ffa6003d329
sunxh <sunxiaohai@kedacom.com>
2019-04-10 17:05:28 +0800

    xiugai

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { ExcelCanvas } from '@src/components/excelCanvas';
-
+import '@src/common/style/index.styl';
 ReactDOM.render(
 	<div>
 		<ExcelCanvas></ExcelCanvas>
